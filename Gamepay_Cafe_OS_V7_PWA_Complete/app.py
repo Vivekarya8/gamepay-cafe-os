e84bbf628503c991049fc6c650736cfd1dafefcb
Vivekarya8 <vivekarya8@gmail.com>
2026-07-22 17:25:55 +0530

    Update app.py

--- Gamepay_Cafe_OS_V7_PWA_Complete/app.py
+++ Gamepay_Cafe_OS_V7_PWA_Complete/app.py
@@ -1058,5 +1058,5 @@ def day_history():
         "day_history.html",
         days=days
     )
-    if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000,debug=False)
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
